6e8c2f7c4a39131570213bec62d0d46bc42ff5c4
KEIDOROa <113253888+KEIDOROa@users.noreply.github.com>
2026-03-20 11:36:20 +0900

    効率化

--- application/client/postcss.config.js
+++ application/client/postcss.config.js
@@ -1,9 +1,11 @@
 const postcssImport = require("postcss-import");
 const postcssPresetEnv = require("postcss-preset-env");
+const tailwindPostcss = require("@tailwindcss/postcss");
 
 module.exports = {
   plugins: [
     postcssImport(),
+    tailwindPostcss(),
     postcssPresetEnv({
       stage: 3,
     }),
